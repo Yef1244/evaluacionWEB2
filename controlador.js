d02b37389eef05f50f42546ae3242edfc7e8903c
Yef1244 <yeferson1244@gmail.com>
2021-09-14 01:12:31 -0500

    Se crea pagina Spotify

--- controlador.js
+++ controlador.js
@@ -6,7 +6,10 @@ let titulo=document.getElementById("titulo")
 let parrafo=document.getElementById("parrafo")
 let parrafo2=document.getElementById("parrafo2")
 
-juegos.addEventListener("click",cambiarJuego)
+
+/*--------------------------------------*/
+/*-----         BOTON INFO         -----*/
+/*--------------------------------------*/
 
 function cambiarJuego(){
     foto.src="img/gears.jpg"
@@ -15,8 +18,6 @@ function cambiarJuego(){
     parrafo2.textContent="Pero ese no es mi unico hobby, también me gusta jugar futbol, ver freestyle (improvisación-batallas) y escuchar musica de cualquier genero aunque mi favorita es el RAP, Reggae junto con algunos clasicos de Reggaeton, estos hobbies han formado parte de mi vida y son causa de mi forma de ser hoy en día, incluso quiero comprar varios instrumentos musicales para practicar."
 }
 
-sobremi.addEventListener("click",cambiarSobremi)
-
 function cambiarSobremi(){
     foto.src="img/sobremi.jpg"
     titulo.textContent="Un poco sobre mí"
@@ -24,14 +25,34 @@ function cambiarSobremi(){
     parrafo2.textContent="pesar de que aún no termino el curso ya pude realizar las practicas en una empresa dedicada a desarrollar formatos para facturas de venta, ya sean electronicas o fisicas, en dicha empresa estuve muy dedicado a trabajar con queryS de MySql."
 }
 
-musica.addEventListener("click",cambiarMusica)
-
 function cambiarMusica(){
     foto.src="img/gona.jpg"
     titulo.textContent="Uno de mis artistas favoritos"
     parrafo.textContent="Llamado Gérman Romero pero es más conocido como GONA, es un cantante venezolano de RAP y reggae que ha tenido una gran trayectoria en los ultimos 10 años llegando a escenarios de Perú, Colombia e incluso España, su álbum más exitoso es `Ocho Lunas` y la canción más exitosa fue `Hasta las 3 de la mañana` perteneciente de Ocho Lunas"
     parrafo2.textContent="Conocí su musica en el 2016 y desde ese entonces no he dejado de escucharlo, también he tenido la oportunidad de conocerlo en un concierto que presento en Medellín hace 3 años."
 }
+
+musica.addEventListener("click",cambiarMusica)
+sobremi.addEventListener("click",cambiarSobremi)
+juegos.addEventListener("click",cambiarJuego)
+
+
+/*--------------------------------------*/
+/*-----       BOTON SPOTIFY        -----*/
+/*--------------------------------------*/
+
+function mostrar(){
+    document.getElementById('botonescondido').style.display ='inline'
+    }
+
+function ocultar(){
+    document.getElementById('botonescondido').style.display ='none'
+    }
+
+musica.addEventListener("click",mostrar)
+sobremi.addEventListener("click",ocultar)
+juegos.addEventListener("click",ocultar)
+
 
 
 
